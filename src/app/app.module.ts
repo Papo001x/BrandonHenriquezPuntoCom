21e016023a6f838088706675e9f7ffb590f3c742
Brandon Steven Henriquez Berrios <brandonpapo001@gmail.com>
2022-04-18 13:04:18 -0500

    version 1 de incio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componets/header/header.component';
+import { InicioComponent } from './componets/inicio/inicio.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    InicioComponent,
+    ExperienciaComponent,
   ],
   imports: [
     BrowserModule,
